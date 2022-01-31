6dbf514985da432413fee92cb9d11ff8577e8402
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-31 14:12:22 +0100

    cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
