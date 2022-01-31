f233cab3047e6f0b2bdf91620aa514c3422138b9
Jakub Kozłowski <kubukoz@gmail.com>
2022-01-31 14:34:19 +0100

    Add helper function for calling smithy4s generate

--- flake.nix
+++ flake.nix
@@ -7,26 +7,47 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; overlays = [ (final: prev: { jre = final.openjdk11; }) ]; };
-        lib = pkgs.callPackage ./lib.nix { };
+        coursier = pkgs.callPackage ./coursier.nix { };
         version = "0.11.3";
       in
       {
         defaultPackage = self.packages.${system}.codegen;
         packages = {
           codegen = pkgs.callPackage ./derivation.nix {
-            inherit (lib) coursier-tools;
+            inherit (coursier) coursier-tools;
             inherit version;
           };
         };
 
         checks = self.packages.${system} // {
           pizza = pkgs.callPackage ./tests/pizza.nix {
-            smithy4s-codegen = self.packages.${system}.codegen;
+            inherit (self.lib.${system}) smithy4sGenerate;
             inherit version;
           };
         };
 
-        inherit lib;
+        lib = {
+          smithy4sGenerate =
+            { pname, version, specs }:
+            let inherit (pkgs) stdenv; in
+
+            stdenv.mkDerivation {
+              pname = "smithy4s-${pname}";
+              inherit version;
+
+              dontUnpack = true;
+              buildInputs = [ self.packages.${system}.codegen ];
+
+              buildPhase = ''
+                smithy4s-codegen generate \
+                  --output $out/scala \
+                  --openapi-output $out/openapi \
+                  ${builtins.concatStringsSep " " specs}
+              '';
+
+              dontInstall = true;
+            };
+        };
       }
     );
 }
--- tests/pizza.nix
+++ tests/pizza.nix
@@ -1,23 +1,12 @@
-{ smithy4s-codegen, stdenv, version }:
+{ smithy4sGenerate, version }:
 
 let pizzaSpec = builtins.fetchurl {
   url = "https://raw.githubusercontent.com/disneystreaming/smithy4s/v${version}/sampleSpecs/pizza.smithy";
   sha256 = "sha256:1m82wkll5wfr8f6m7hd4s3cs3f2zskzs8448hxs10d24wlspfg0w";
 };
 in
-stdenv.mkDerivation {
+smithy4sGenerate {
   pname = "pizza";
   inherit version;
-
-  dontUnpack = true;
-  buildInputs = [ smithy4s-codegen ];
-
-  buildPhase = ''
-    smithy4s-codegen generate \
-      --output $out/scala \
-      --openapi-output $out/openapi \
-      ${pizzaSpec}
-  '';
-
-  dontInstall = true;
+  specs = [ pizzaSpec ];
 }
