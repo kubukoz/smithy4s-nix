a200a3a6c683eb6a18ce7f6f889749f1031683b1
Jakub Kozłowski <kubukoz@gmail.com>
2022-03-31 03:10:35 +0200

    Add overlay

--- flake.nix
+++ flake.nix
@@ -3,31 +3,38 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ (final: prev: { jre = final.openjdk11; }) ]; };
-        coursier = pkgs.callPackage ./coursier.nix { };
-        version = "0.12.7";
-      in
-      {
-        defaultPackage = self.packages.${system}.codegen;
-        packages = {
-          codegen = pkgs.callPackage ./derivation.nix {
-            inherit (coursier) coursier-tools;
-            inherit version;
+    let version = "0.12.7"; in
+
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+
+        let pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; }; in
+        {
+          defaultPackage = self.packages.${system}.codegen;
+          packages =
+            pkgs.smithy4s;
+
+          checks = self.packages.${system} // {
+            pizza = pkgs.callPackage ./tests/pizza.nix {
+              inherit (self.lib.${system}) smithy4sGenerate;
+              inherit version;
+            };
+          };
+
+          lib = pkgs.callPackage ./lib.nix { smithy4s-codegen = self.packages.${system}.codegen; };
+        }
+      ) // {
+      overlays.default = final: _:
+        let coursier = final.callPackage ./coursier.nix { }; in
+        {
+          smithy4s = {
+            codegen = final.callPackage ./derivation.nix {
+              inherit (coursier) coursier-tools;
+              inherit version;
+            };
           };
         };
-
-        checks = self.packages.${system} // {
-          pizza = pkgs.callPackage ./tests/pizza.nix {
-            inherit (self.lib.${system}) smithy4sGenerate;
-            inherit version;
-          };
-        };
-
-        lib = pkgs.callPackage ./lib.nix { smithy4s-codegen = self.packages.${system}.codegen; };
-      }
-    );
+    };
 }
 
