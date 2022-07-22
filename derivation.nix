fed12dea85245c48e5a387ca2e35b784ae099d60
Jakub Kozłowski <kubukoz@gmail.com>
2022-07-22 18:43:38 +0200

    Update to 0.14.2

--- derivation.nix
+++ derivation.nix
@@ -1,16 +1,16 @@
 { coursier-tools }:
 let
   pname = "smithy4s-codegen";
-  version = "0.13.0";
+  version = "0.14.2";
   channel = builtins.fromJSON (builtins.readFile (builtins.fetchurl {
     url = "https://disneystreaming.github.io/coursier.json";
-    sha256 = "sha256:1wzv32gd653gdr45mmxrdpljcjyw4bii00c268hm35mxbnmlfid5";
+    sha256 = "1wzv32gd653gdr45mmxrdpljcjyw4bii00c268hm35mxbnmlfid5";
   }));
 in
 coursier-tools.coursierBootstrap {
   inherit version pname;
   artifact = "com.disneystreaming.smithy4s::smithy4s-codegen-cli:${version}";
   inherit (channel.smithy4s) mainClass;
-  sha256 = "sha256-9JquKFyNBOJY6CYNWQ+MU538UwZAROs4PwsBWkQcX90=";
+  sha256 = "sha256-nWyoqmX/L43dewOwWbUuKLr2TSe36aiRcoEGPWukmCw";
 }
 
